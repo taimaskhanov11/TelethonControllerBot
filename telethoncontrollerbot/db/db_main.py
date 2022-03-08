10ffc5593aae9f85f86dd4688144111c17e200c5
taimaskhanov11 <81189712+taimaskhanov11@users.noreply.github.com>
2022-03-08 11:47:01 +0300

    ver 0.1.2.1 released minor fix

--- telethoncontrollerbot/db/db_main.py
+++ telethoncontrollerbot/db/db_main.py
@@ -8,14 +8,22 @@ from telethoncontrollerbot.config.config import DB_USERNAME, DB_PASSWORD, DB_HOS
 
 async def init_tortoise(username=DB_USERNAME, password=DB_PASSWORD, host=DB_HOST, port=DB_PORT, db_name=DB_DB_NAME):
     logger.debug(f"Инициализация BD {host}")
-    await Tortoise.init(  # todo
-        # _create_db=True,
-        db_url=f"postgres://{username}:{password}@{host}:{port}/{db_name}",
-        modules={"models": ["telethoncontrollerbot.db.models"]},
-    )
-    await Tortoise.generate_schemas()
-    logger.debug(f"База данных {db_name} инициализирована")
-
-
+    try:
+        await Tortoise.init(  # todo
+            _create_db=True,
+            db_url=f"postgres://{username}:{password}@{host}:{port}/{db_name}",
+            modules={"models": ["telethoncontrollerbot.db.models"]},
+        )
+        await Tortoise.generate_schemas()
+        logger.debug(f"База данных {db_name} инициализирована")
+    except Exception as e:
+        logger.critical(e)
+        await Tortoise.init(  # todo
+            # _create_db=True,
+            db_url=f"postgres://{username}:{password}@{host}:{port}/{db_name}",
+            modules={"models": ["telethoncontrollerbot.db.models"]},
+        )
+        await Tortoise.generate_schemas()
+        logger.debug(f"База данных {db_name} инициализирована")
 if __name__ == "__main__":
     asyncio.run(init_tortoise())
